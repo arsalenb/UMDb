082d5cb6facdf0f7135ba8b77815195e4d1d0e55
Arselene <arselenebidani@yahoo.fr>
2022-04-21 06:25:39 +0200

    New push

---
+++ backend/models/movie.js
@@ -0,0 +1,57 @@
+const mongo = require("mongoose");
+
+const Schema = mongo.Schema;
+
+const movieSchema = new Schema({
+  budget: {
+    type: Number,
+  },
+  cast: {
+    type: Array,
+    default: [],
+  },
+  genres: {
+    type: Array,
+  },
+  _id: {
+    type: String,
+  },
+  overview: {
+    type: String,
+  },
+  popularity: {
+    type: Number,
+  },
+  poster_path: {
+    type: String,
+  },
+  release_date: {
+    type: String,
+  },
+  revenue: {
+    type: Number,
+  },
+  runtime: {
+    type: Number,
+  },
+  spoken_languages: {
+    type: Array,
+    default: [],
+  },
+  title: {
+    type: String,
+  },
+  vote_average: {
+    type: Number,
+    default: 0,
+  },
+  vote_count: {
+    type: Number,
+    default: 0,
+  },
+  reviews: {
+    type: Array,
+    default: [],
+  },
+});
+module.exports = mongo.model("Movie", movieSchema);
