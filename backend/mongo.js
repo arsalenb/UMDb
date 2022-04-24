b7da52ad151e6f87a0db595b5e5cddfbc36aac40
Arsalen <45715272+Arselene@users.noreply.github.com>
2022-04-24 23:51:24 +0200

    Update mongo.js

--- backend/mongo.js
+++ backend/mongo.js
@@ -6,7 +6,7 @@ async function mongo() {
   if (client == null) {
     const uri =
       "mongodb://172.16.4.65:27020,172.16.4.64:27020,172.16.4.60:27020/";
-    client = new MongoClient(uri, { w: 1, readPreference: "primaryPreferred" });
+    client = new MongoClient(uri, { w: 1, readPreference: "nearest" });
     // const uri = "mongodb://localhost:27017";
     // client = new MongoClient(uri);
     await client.connect();
